3d8dff79b761015324d941c653099badc3591a28
sauchkin967 <sauchkin967@gmail.com>
2018-12-02 16:06:05 +0200

    Update2.0

---
+++ Графический редактор/MainForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Графический_редактор
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(2, 44);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(77, 49);
+            this.btnColor.TabIndex = 1;
+            this.btnColor.Text = "Выбор цвета";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click_1);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(2, 99);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(77, 48);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // openButton
+            // 
+            this.openButton.Image = ((System.Drawing.Image)(resources.GetObject("openButton.Image")));
+            this.openButton.Location = new System.Drawing.Point(2, 3);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(35, 33);
+            this.openButton.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.openButton, "Открыть файл\r\n");
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(85, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(571, 349);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(2, 320);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(77, 32);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.Location = new System.Drawing.Point(43, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(36, 33);
+            this.btnSave.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.btnSave, "Сохранить файл\r\n");
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 360);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnColor);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Графический редактор";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Графический редактор/MainForm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Графический_редактор
+{
+    public partial class MainForm : Form
+    {
+        Color CurrentColor = Color.Black;
+        bool isPressed = false;
+        Point CurrentPoint;
+        Point PrevPoint;
+        Graphics g;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void for_paint()
+        {
+            Pen p = new Pen(CurrentColor);
+            g.DrawLine(p, PrevPoint, CurrentPoint);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.JPG; *.PNG; *.GIF; *.BMP; *.ICO) | *.JPG; *.PNG; *.GIF; *.BMP; *.ICO | All files (*.*) | *.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+                try
+                {
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+
+                }
+                catch
+                {
+                    MessageBox.Show("Невозмможно открыть файл","Ошибка",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+        }
+
+        private void btnColor_Click_1(object sender, EventArgs e)
+        {
+            DialogResult D = colorDialog1.ShowDialog();
+            if (D == System.Windows.Forms.DialogResult.OK)
+            {
+                CurrentColor = colorDialog1.Color;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                PrevPoint = CurrentPoint;
+                CurrentPoint = e.Location;
+                for_paint();
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPressed = true;
+            CurrentPoint = e.Location;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Завершить работу редактора?",
+             "Завершить работу", MessageBoxButtons.OKCancel,
+              MessageBoxIcon.Question) == DialogResult.OK)
+                Application.Exit();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            
+            
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Сохранить картинку как";
+                sfd.OverwritePrompt = true;
+                sfd.CheckPathExists = true;
+                sfd.Filter = "Image files(*.PNG)| *.PNG | Image files(*.JPG) |*.JPG| Image files(*.BMP) |*.BMP|" +
+                    " All files(*.*) | *.* ";
+                sfd.ShowHelp = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image.Save(sfd.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно сохранить изображение", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+    }
+}
